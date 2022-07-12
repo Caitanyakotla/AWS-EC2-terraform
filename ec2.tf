41c990d4c20bf6b024afed8b5430f1704dc753a5
Caitanyakotla <kotlakrishna520@gmail.com>
2022-07-12 21:45:22 +0200

    instance size changed

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-0e472ba40eb589f49"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   subnet_id     = "subnet-0b4615cdf94395cda"
   #security_groups = [aws_security_group.TerraformEc2_security.name]
   key_name = "krishnakotla-key"
